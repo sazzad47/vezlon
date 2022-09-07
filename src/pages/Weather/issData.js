9d76631c3b1f1bfcc447f243b18e3e1b499484e4
Sazzad  Hossen <sazzad47.ju@gmail.com>
2022-09-07 18:31:33 +0600

    make it better

--- src/pages/Weather/issData.js
+++ src/pages/Weather/issData.js
@@ -181,8 +181,8 @@ const ISSData = ({setOperatorLat, setOperatorLong, setOperatorCity, setCoords, s
 
   function processData(data) {
     // Here, you can put your code to process the data from response
-      let lat = data.latitude;
-      let long = data.longitude;
+      let lat = parseFloat(data.latitude);
+      let long = parseFloat(data.longitude);
       let currentCity = data.city;
       setOperatorLat(lat);
       setOperatorLong(long);
