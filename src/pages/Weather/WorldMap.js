24e1b77636088aaeff7a672ca058d4a8f3847764
Sazzad  Hossen <sazzad47.ju@gmail.com>
2022-09-07 21:16:08 +0600

    make it better

--- src/pages/Weather/WorldMap.js
+++ src/pages/Weather/WorldMap.js
@@ -5,12 +5,15 @@ import { MapContainer, Polyline, Popup, TileLayer, Tooltip, useMap } from 'react
 import { FullscreenControl } from "react-leaflet-fullscreen";
 import Satellite from '../../assets/images/satellite.png';
 import FullScreenDropdown from '../../Components/Common/FullScreenDropdown';
+import useGeoLocation from './useGeoLocation';
 
 
-const WorldMap = ({operatorLat, operatorLong, operatorCity, coords, latitude, longitude }) => {
- 
+const WorldMap = ({coords, latitude, longitude }) => {
+  const location = useGeoLocation();
   const coord = [Number(latitude), Number(longitude)];
-  const operatorPosition = [Number(operatorLat), Number(operatorLong)];
+  const operatorLat = location.loaded? location.coordinates.lat : null;
+  const operatorLong = location.loaded? location.coordinates.lng : null;
+  const operatorPosition = [operatorLat, operatorLong];
   
  
   
@@ -42,12 +45,12 @@ const WorldMap = ({operatorLat, operatorLong, operatorCity, coords, latitude, lo
             </Tooltip>
             <Popup>INTERNATIONAL SPACE STATION LIVE COORDINATES</Popup>
           </Marker>
-          {(operatorLat && operatorLong && operatorCity) && <Marker
+          {location.loaded && <Marker
            position = {operatorPosition}
           
            >
           <Tooltip direction="bottom" offset={[20, 20]} opacity={1} permanent>
-            You are here in {operatorCity} <br/> Latitude: {operatorLat?.toFixed(1)} ° <br/> Longitude: {operatorLong?.toFixed(1)} °
+            You are here <br/> Latitude: {operatorLat?.toFixed(1)} ° <br/> Longitude: {operatorLong?.toFixed(1)} °
             </Tooltip>
           </Marker>}
          <Polyline positions={coords} color="red" />
