d27fe29cdd75d2c818a17c782081cc1c5e511af6
Sazzad  Hossen <sazzad47.ju@gmail.com>
2022-09-14 00:14:15 +0600

    make it better

--- src/pages/Weather/Cameras.js
+++ src/pages/Weather/Cameras.js
@@ -3,8 +3,8 @@ import ISSStream from './ISSStream';
 
 const breakPoints = [
   { width: 1, itemsToShow: 1 },
-  { width: 550, itemsToShow: 2, itemsToScroll: 2 },
-  { width: 768, itemsToShow: 2 },
+  { width: 550, itemsToShow: 3, itemsToScroll: 3 },
+  { width: 768, itemsToShow: 3 },
   { width: 1200, itemsToShow: 3 }
 ];
 const Cameras = () => {
@@ -12,7 +12,7 @@ const Cameras = () => {
           <div style={{display:'flex', flexDirection:'column'}}>
          <div 
             >
-              <Carousel style={{ minHeight:'15rem', position:'relative'}}  breakPoints={breakPoints} >
+              <Carousel itemPadding={[0, 10]} outerSpacing={0} style={{ minHeight:'15rem', position:'relative'}}  breakPoints={breakPoints} >
                  <div style={{minHeight:'16rem'}}>
                  <ISSStream width="100%" height="100%" />
                  </div>
@@ -40,7 +40,7 @@ const Cameras = () => {
             </div>
          <div 
             >
-              <Carousel style={{ minHeight:'15rem', position:'relative', marginTop:'10px'}}  breakPoints={breakPoints} >
+              <Carousel itemPadding={[0, 10]} outerSpacing={0} style={{ minHeight:'15rem', position:'relative', marginTop:'10px'}}  breakPoints={breakPoints} >
                  <div style={{minHeight:'16rem'}}>
                  <ISSStream width="100%" height="100%" />
                  </div>
